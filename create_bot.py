ef1e99cf484651e5d9688c17945306eb1b5a9849
Neivo <126583090+VladislavXD@users.noreply.github.com>
2024-10-24 21:12:32 +0500

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,9 @@
 import telebot
 import os
 from Database import Database
+from handlers.keep_alive import keep_alive
 
+keep_alive()
 
 db = Database('./database.db')
 bot = telebot.TeleBot(token=os.environ.get('TOKEN'))
