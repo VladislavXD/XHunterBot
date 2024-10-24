e9fc7a1cf0abf397356a1948898c382d7b5673bb
Neivo <126583090+VladislavXD@users.noreply.github.com>
2024-10-24 20:55:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from create_bot import bot
 import handlers.main_handler
 import handlers.start_handler
 import handlers.create_bot.add_bot
-from handlers.keep_alive import keep_alive
 
-keep_alive()
 
 
 
