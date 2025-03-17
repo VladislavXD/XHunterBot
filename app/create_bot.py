89b5552367f2e89f3ef0795f01fc999bfaf00a96
Vladislav <neivo@vladislav.dev>
2025-03-17 22:20:15 +0500

    init commit to heroku

--- app/create_bot.py
+++ app/create_bot.py
@@ -2,12 +2,12 @@ import asyncio
 import os
 from telebot.async_telebot import AsyncTeleBot
 from Database import Database  # Если `Database` работает синхронно, нужно переделать
-from keep_alive import keep_alive
+# from keep_alive import keep_alive
 from dotenv import load_dotenv
 
 load_dotenv()
 
-keep_alive() 
+# keep_alive() 
 
 db = Database('./database.db')
 bot = AsyncTeleBot(token=os.getenv('TOKEN'))
