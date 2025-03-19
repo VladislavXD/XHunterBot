417155f646ab94005a4fe85248e712a28c457d9f
Vladislav <neivo@vladislav.dev>
2025-03-19 17:16:56 +0500

    update keep_alive for heroku

--- app/create_bot.py
+++ app/create_bot.py
@@ -2,10 +2,10 @@ import asyncio
 import os
 from telebot.async_telebot import AsyncTeleBot
 from Database import Database  # Если `Database` работает синхронно, нужно переделать
-from keep_alive import keep_alive
+
 from dotenv import load_dotenv
 
-load_dotenv()
+
 
 
 
