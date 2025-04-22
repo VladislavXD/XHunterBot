60f610d9f07663cda42a4104863a93853416551e
Vladislav <neivo@vladislav.dev>
2025-04-23 01:13:06 +0500

    update middleware

--- app/middleware/middleware.py
+++ app/middleware/middleware.py
@@ -33,28 +33,36 @@ last_click_time = {}
 
 
 def check_subscription_decorator(func):
-    async def wrapper( *args, **kwargs):
-        message = args[0]  # Первым аргументом всегда будет message или call
-        try:
-            # Определяем user_id
-            if hasattr(message, 'from_user'):
-                user_id = message.from_user.id
-                chat_id = message.chat.id
-            elif hasattr(message, 'message') and hasattr(message.message, 'from_user'):
-                user_id = message.from_user.id
-                chat_id = message.message.chat.id
-            else:
-                return  # Невозможно определить пользователя
+    from telebot.types import Message, CallbackQuery
 
-            # Проверяем подписку
+    async def wrapper(*args, **kwargs):
+        # Ищем объект message или call
+        message = None
+        user_id = None
+
+        for arg in args:
+            if isinstance(arg, Message):
+                message = arg
+                user_id = message.from_user.id
+                break
+            elif isinstance(arg, CallbackQuery):
+                message = arg.message
+                user_id = arg.from_user.id
+                break
+
+        if not user_id:
+            print("[Ошибка декоратора] Невозможно определить пользователя")
+            return
+
+        try:
             member = await bot.get_chat_member(chat_id='-1001832025300', user_id=user_id)
             if member.status in ['member', 'administrator', 'creator']:
-                return await func(message, *args, **kwargs)
+                return await func(*args, **kwargs)
             else:
-                await bot.send_message(user_id, "Вы не подписаны на канал.", reply_markup=createButtonChannel())
-
+                await bot.send_message(user_id, "Пожалуйста, подпишитесь на канал для продолжения.", reply_markup=createButtonChannel())
         except Exception as e:
-            await bot.send_message(user_id, f"Ошибка. Попробуйте повторить позже.\n\n{e}")
+            await bot.send_message(user_id, f"Ошибка при проверке подписки.\n\n{e}")
+            print(f"[Ошибка проверки подписки] user_id={user_id} — {e}")
 
     return wrapper
 
